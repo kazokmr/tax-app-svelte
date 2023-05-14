5dd8197171818ee889ed2a7347a0afa1acd8f87c
kazokmr <engkokmr@gmail.com>
2023-05-14 20:48:07 +0900

    5.3.4 API に対するテスト

--- tax-app-experiment/src/index.ts
+++ tax-app-experiment/src/index.ts
@@ -1,30 +1,6 @@
-import express from "express";
+import app from "./app";
 
-const app = express();
 const port = 3000;
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.post("/check-post", (req, res) => {
-  res.send("Hello Post!");
-});
-
-app.post("/check-json", (req, res) => {
-  res.json({ message: "Hello JSON!" });
-});
-
-app.post("/check-status", (req, res) => {
-  res.status(500).json({ message: "Hello StatusCode!" });
-});
-
-app.post("/check-body", (req, res) => {
-  console.dir(req.body);
-  res.json({ message: "Hello JSON body!" });
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
