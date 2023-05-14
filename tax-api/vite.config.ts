42c3698019b3e2d02cc41ba91194a933acde8e15
kazokmr <engkokmr@gmail.com>
2023-05-14 21:48:23 +0900

    5.6 initial commit.

---
+++ tax-api/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vitest/config";
+import { VitePluginNode } from "vite-plugin-node";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    coverage: {
+      provider: "c8",
+      reporter: ["html"],
+      reportsDirectory: "./reports/coverage",
+    },
+    reporters: [
+      "default",
+      "junit",
+    ],
+    outputFile: "./reports/vitest/vitest-report.xml",
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    ...VitePluginNode({
+      adapter: "express",
+      appPath: "./src/index.ts",
+      exportName: "viteNodeApp",
+      tsCompiler: "esbuild",
+      swcOptions: {},
+    }),
+  ],
+});
