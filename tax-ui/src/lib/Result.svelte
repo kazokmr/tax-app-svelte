ce589c0309c8997e7c7bce7a57b1643b63b9b0cd
kazokmr <engkokmr@gmail.com>
2023-05-26 20:50:45 +0900

    6.2.4 複数の状態を確認する

---
+++ tax-ui/src/lib/Result.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Card, CardBody, CardHeader, CardTitle } from "sveltestrap";
+
+  export let tax: number | null = null;
+  $: taxStr = tax === null ? "---" : new Intl.NumberFormat("ja-JP").format(tax);
+</script>
+
+<Card style="height: 200px; width: 400px">
+  <CardHeader>
+    <div class="text-center">
+      <CardTitle>退職金にかかる所得税</CardTitle>
+    </div>
+  </CardHeader>
+  <CardBody>
+    <div class="text-center">
+      <span class="fs-1">{taxStr}</span>
+      <span>円</span>
+    </div>
+  </CardBody>
+</Card>
