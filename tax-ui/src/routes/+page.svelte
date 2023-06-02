34a711c863eab4ddc024f62ee8c23b07b5482b5a
kazokmr <engkokmr@gmail.com>
2023-06-03 07:59:03 +0900

    6.4.3 ページコンポーネントからAPIを呼び出す

--- tax-ui/src/routes/+page.svelte
+++ tax-ui/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import type { InputForm } from "$lib/components/InputForm.svelte";
   import Presentation from "$lib/components/Presentation.svelte";
+  import type { CalcTaxParam, CalcTaxResult } from "$lib/fetch/client/calcTax";
+  import { calcTax } from "$lib/fetch/client/calcTax";
 
   const formInputs = {
     yearsOfService: 10,
@@ -9,10 +11,18 @@
     severancePay: 5_000_000
   } satisfies InputForm;
 
-  const handleInputFormSubmit = (event) => {
+  const handleInputFormSubmit = async (event) => {
     const formData = new FormData(event.target);
-    for (let data of formData.entries()) {
-      console.log(data);
+    const param = {
+      yearsOfService: Number(formData.get("yearsOfService")),
+      isDisability: !!formData.get("isDisability"),
+      isOfficer: !!Number(formData.get("isOfficer")),
+      severancePay: Number(formData.get("severancePay"))
+    }satisfies CalcTaxParam;
+    const response = await calcTax(param);
+    if (response.ok) {
+      const json = await response.json() satisfies CalcTaxResult;
+      console.log(json);
     }
   };
 
