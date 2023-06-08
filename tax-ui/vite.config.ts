0088b2c77719a2c2a1a5cb823777152f22d48718
kazokmr <engkokmr@gmail.com>
2023-06-08 15:21:36 +0900

    Superformsが利用できるように、SvelteKitの Server-side の runtime modules の Mocking設定を追加した。

--- tax-ui/vite.config.ts
+++ tax-ui/vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
-    include: ["src/**/*.{test,spec}.{js,ts}"],
+    globals: true,
     environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,ts}"],
     setupFiles: ["./vitest.setup.ts"],
-    globals: true
+    exclude: [...configDefaults.exclude, "test"]
   }
 });
--- tax-ui/vitest.setup.ts
+++ tax-ui/vitest.setup.ts
@@ -1,1 +1,75 @@
-import "@testing-library/jest-dom";
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { expect, vi } from "vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import * as environment from "$app/environment";
+import * as navigation from "$app/navigation";
+import * as stores from "$app/stores";
+import { readable } from "svelte/store";
+import { Navigation, Page } from "@sveltejs/kit";
+
+expect.extend(matchers);
+
+vi.mock("$app/environment", (): typeof environment => ({
+  browser: false,
+  dev: false,
+  building: false,
+  version: "any"
+}));
+
+vi.mock("$app/navigation", (): typeof navigation => ({
+  afterNavigate: () => {
+  },
+  beforeNavigate: () => {
+  },
+  disableScrollHandling: () => {
+  },
+  goto: () => Promise.resolve(),
+  invalidate: () => Promise.resolve(),
+  invalidateAll: () => Promise.resolve(),
+  preloadData: () => Promise.resolve(),
+  preloadCode: () => Promise.resolve()
+}));
+
+vi.mock("$app/stores", (): typeof stores => {
+  const getStores: typeof stores.getStores = () => {
+    const navigating = readable<Navigation | null>(null);
+    const page = readable<Page>({
+      url: new URL("http://localhost"),
+      params: {},
+      route: {
+        id: null
+      },
+      status: 200,
+      error: null,
+      data: {},
+      form: undefined
+    });
+    const updated = { subscribe: readable(false).subscribe, check: async () => false };
+
+    return { navigating, page, updated };
+  };
+
+  const page: typeof stores.page = {
+    subscribe(fn) {
+      return getStores().page.subscribe(fn);
+    }
+  };
+  const navigating: typeof stores.navigating = {
+    subscribe(fn) {
+      return getStores().navigating.subscribe(fn);
+    }
+  };
+  const updated: typeof stores.updated = {
+    subscribe(fn) {
+      return getStores().updated.subscribe(fn);
+    },
+    check: async () => false
+  };
+
+  return {
+    getStores,
+    navigating,
+    page,
+    updated
+  };
+});
